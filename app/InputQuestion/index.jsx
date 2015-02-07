547ebd726742d7d73bd6424b604a2a920690b7d4
Johannes Schickling <schickling.j@gmail.com>
2015-02-07 21:03:18 +0000

    interpolation

--- app/InputQuestion/index.jsx
+++ app/InputQuestion/index.jsx
@@ -9,9 +9,16 @@ module.exports = React.createClass({
   },
 
   render: function() {
+    var text = this.props.question.text;
+    var tokenIndex = text.indexOf('%!');
+    var beforeToken = text.substr(0, tokenIndex);
+    var afterToken = text.substr(tokenIndex + 2);
+    console.log(beforeToken, afterToken);
     return (
       <div>
-        {this.props.question.text} <input type="text" />
+        <span>{beforeToken}</span>
+        <input type="text" />
+        <span>{afterToken}</span>
       </div>
     );
   },
